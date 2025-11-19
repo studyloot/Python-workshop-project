4a5141b442936829db83ce626742e571fd793278
Jatin-adlak <jaiadlakwww@gmail.com>
2025-11-19 12:24:40 +0530

    setting changes

--- courseproject/courseproject/settings.py
+++ courseproject/courseproject/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,3 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#for file uploading 
+
+MEDIA_URL='/media/'
+MEDIS_ROOT=os.path.join(BASE_DIR,'media')
